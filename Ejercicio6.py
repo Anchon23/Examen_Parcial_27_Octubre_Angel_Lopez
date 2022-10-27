088bf30f12a4ff630a002c3ecf8e642bac61feaa
Anchon23 <angellopezrouanet@gmail.com>
2022-10-27 20:57:22 +0200

    eje 6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def main():
-    matriz = np.array([(2,0,9),(1,8,3),(2,4,5),(8,2,3),(2,5,1)])
+    matriz = np.diag([(1,4,7,1,7)])
     determinante = np.linalg.det(matriz)
     print("El determinante de la matriz es: %f" % determinante)
 if __name__== "__main__":
