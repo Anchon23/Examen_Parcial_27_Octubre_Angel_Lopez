c5556286213884f99b1e23af2d446d2e1fed8c50
Anchon23 <angellopezrouanet@gmail.com>
2022-10-27 19:39:28 +0200

    metodo calificacion

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -4,5 +4,11 @@ class Alumno():
         self.nombre = nombre
         self.nota = nota
         print(self.nombre,", el alumno, ha sido creado con éxito")
+
+    def calificacion(self):
+        if self.nota>=5:
+            print("aprobar")
+        else:
+            print("suspender")
     
 Albaro = Alumno("Albaro",16)
