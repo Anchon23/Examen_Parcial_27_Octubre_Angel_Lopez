b76ea9fc068a11fac0d2dc40212f9fc4a02aa586
Anchon23 <angellopezrouanet@gmail.com>
2022-10-27 20:10:42 +0200

    crea un nuevo objeto e imprimir en str

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,14 +1,18 @@
 class Alumno():
 
-    def __init__(self,nombre,nota):
+    def __init__(self,nombre,nota,altura,peso):
         self.nombre = nombre
         self.nota = nota
+        self.altura = altura
+        self.peso = peso
         print(self.nombre,", el alumno, ha sido creado con éxito")
 
     def __str__(self):
         return '''\
     nombre\t{}
-    nota\t{}'''.format(self.nombre,self.nota)
+    nota\t{}
+    altura\t{}
+    peso\t{}'''.format(self.nombre,self.nota,self.altura,self.peso)
 
     def calificacion(self):
         if self.nota>=5:
@@ -16,18 +20,18 @@ class Alumno():
         else:
             print(self.nombre,"Suspenso")
         
-Albaro = Alumno("Albaro",6)
+Albaro = Alumno("Albaro",6,1.75,78)
 Albaro.calificacion()
 print(Albaro)
-Pablo = Alumno("Pablo",2)
+Pablo = Alumno("Pablo",2,1.89,97)
 Pablo.calificacion()
 print(Pablo)
-Pepe = Alumno("Pepe",8)
+Pepe = Alumno("Pepe",8,1.54,67)
 Pepe.calificacion()
 print(Pepe)
-Maria = Alumno("Maria",10)
+Maria = Alumno("Maria",10,1.51,43)
 Maria.calificacion()
 print(Maria)
-Nicolas = Alumno("Nicolas",1)
+Nicolas = Alumno("Nicolas",1,1.99,102)
 Nicolas.calificacion()
 print(Nicolas)
